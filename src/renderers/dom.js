e4b389ae90aa03060d57ef568234196d3e005916
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-05-30 00:43:53 -0500

    webFontLoader manager added

--- src/renderers/dom.js
+++ src/renderers/dom.js
@@ -5,6 +5,10 @@ import App from '../App/App';
 
 import '../styles/index.css';
 
+import WebFont from 'webfontloader';
+
+WebFont.load({ google: { families: ['Tangerine:48', 'serif'] } });
+
 ReactDOM.hydrate(
   <App initialData={window.__R_DATA.initialData} />,
   document.getElementById('root')
