b6fe8e1a6c30db722e4e670bdb0b891586ce461a
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-06-03 20:32:10 -0500

    webpack images management added

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ const config = {
         exclude: /node_modules/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000',
+      },
     ],
   },
   optimization: {
