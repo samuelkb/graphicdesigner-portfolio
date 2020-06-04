3d1329029986ebb0cad009fb102e11be5ee7016f
Hernandez Carmona Samuel Karim <mb78200@MB78200MAC.local>
2020-06-03 20:50:08 -0500

    Novedades section added

---
+++ src/components/Novedades.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Novedades() {
+  return (
+    <section id="novedades">
+      <div>
+        <h2>Vista de Novedades</h2>
+        <p>En construcción.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Novedades;
